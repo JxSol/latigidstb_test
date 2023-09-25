148d94886ca672ed615dc2727d40549d7564b1cb
JxSol <jxsol.js@gmail.com>
2023-09-26 01:19:17 +0600

    feat(robots): add admin.py

--- robots/admin.py
+++ robots/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
-# Register your models here.
+from robots.models import Robot
+
+@register(Robot)
+class RobotAdmin(admin.ModelAdmin):
+    list_display = ('serial', 'created')
+    date_hierarchy = 'created'
