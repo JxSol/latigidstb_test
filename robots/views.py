2f489cea521d09d34bda6c42f1ee1fbb481efd4d
JxSol <jxsol.js@gmail.com>
2023-09-28 15:01:45 +0600

    feat(robots): add ProductionWeekReport

--- robots/views.py
+++ robots/views.py
@@ -1,13 +1,17 @@
 import json
 
+from django.db.models import Count
 from django.forms import model_to_dict
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
-from .messages import JSON_400, ROBOT_201
+from .messages import JSON_400
 from .models import Robot
+from .paginators import RobotModelPaginator
+from .utils import generate_workbook
 
 
 @method_decorator(csrf_exempt, name="dispatch")
@@ -50,3 +54,45 @@ class CreateRobotView(CreateView):
         """Переопределяем неуспешный ответ."""
         super().form_invalid(form)
         return JsonResponse({'detail': form.errors}, status=400)
+
+
+class ProductionWeekReport(ListView):
+    """
+    Формирует Excel-отчёт по созданию роботов за последнюю неделю.
+
+    Форма отчёта:
+        Модель | Версия | Количество за неделю
+    Пагинация по моделям.
+    """
+    # день, с которого начинается отчёт
+    date_from = timezone.now() - timezone.timedelta(days=7)
+    filename = f'robot-production-week-report-{date_from.date()}.xlsx'
+
+    queryset = (
+        Robot.objects
+        .filter(created__gte=date_from)
+        .values('model', 'version')
+        .annotate(counter=Count('*'))
+    )
+    fields = {
+        'model': 'Модель',
+        'version': 'Версия',
+        'counter': 'Количество за неделю'
+    }
+    paginator_class = RobotModelPaginator
+    response_class = HttpResponse
+    content_type = 'application/vnd.ms-excel'
+
+    def render_to_response(self, context, **response_kwargs):
+        """Переопределяем рендеринг данных для ответа."""
+        response_kwargs.setdefault("content_type", self.content_type)
+        wb = generate_workbook(
+            list(self.fields.keys()),
+            list(self.fields.values()),
+            self.paginator_class(self.queryset),
+        )
+        response = self.response_class(**response_kwargs)
+        response[
+            'Content-Disposition'] = f'attachment; filename={self.filename}'
+        wb.save(response)
+        return response
